6df8a84b9afd4f63490a0ebb0660d4b607f89c03
YahyaAmin <yahyaamin1997@gmail.com>
2022-07-04 15:39:53 +0500

    Chnages

--- src/test/java/Step_Definitions/SignupSteps.java
+++ src/test/java/Step_Definitions/SignupSteps.java
@@ -62,7 +62,7 @@ public class SignupSteps {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(phone_number_field_at_register_yourself_page)));
         SignupPage.get_phone_number_field_at_register_yourself_page().sendKeys(getRandomNumberLowerAndUpperBound(9,10)+getRandomNumberLowerAndUpperBound(3,4));
 
-        while (driver.findElement(By.xpath("//android.view.ViewGroup[2]/android.widget.EditText")).getText().length()<9) {
+        while (get_phone_number_field_at_register_yourself_page().getText().length()<9) {
             SignupPage.get_phone_number_field_at_register_yourself_page().sendKeys(getRandomNumberLowerAndUpperBound(9,10)+getRandomNumberLowerAndUpperBound(3,4));
         }
         }
