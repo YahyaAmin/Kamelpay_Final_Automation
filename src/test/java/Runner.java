af60b33c46abf4c5225b4f29832a961cbe9a5dd8
YahyaAmin <yahyaamin1997@gmail.com>
2022-07-29 14:58:26 +0500

    Update Runner.java

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
                   "pretty", "json:target/Reports/JSONReport/report.json",
                   "pretty", "junit:target/Reports/XMLReports/report.xml",
                   "json:target/cucumber.json" },
-        tags = "@PayBillTest"
+        tags = "@Sendmoney"
 )
 
 //Example of logical tags:     tags = "@smoketest or @regression"
