bdf45edabd9858f0f72b5662ed61b4c40d3068fc
YahyaAmin <yahyaamin1997@gmail.com>
2022-08-11 17:32:44 +0500

    Changes

--- src/test/java/Hooks/RunnerForGoodWindows.java
+++ src/test/java/Hooks/RunnerForGoodWindows.java
@@ -3,12 +3,15 @@ package Hooks;
 public class RunnerForGoodWindows {
 
     public static void mvnCommandRunner(){
-        GoodWindowsExec.main(new String[] {"cd C:\\Users\\abc\\Documents\\GitHub\\Kamelpay_Final_Automation"});
-        GoodWindowsExec.main(new String[] {"mvn verify -DskipTests"});
+        String [] arr = new String[2];
+        arr [0] = "cd C:\\Users\\abc\\Documents\\GitHub\\Kamelpay_Final_Automation";
+        arr [1] = "mvn verify -DskipTests";
+        //GoodWindowsExec.main(arr);
+        GoodWindowsExec.main(new String [] {"cd C:\\Users\\abc\\Documents\\GitHub\\Kamelpay_Final_Automation"  "mvn verify -DskipTests"});
+        //GoodWindowsExec.main(new String[] {"mvn verify -DskipTests"});
     }
 
     public static void main(String[] args) {
-
         mvnCommandRunner();
     }
 
